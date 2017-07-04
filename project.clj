b1dba166debfcd9d1594907b18e6ffdcdfb404fe
Tony Kay <tony.kay@gmail.com>
2017-07-04 00:58:40 -0700

    added min lein version for heroku

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject untangled-template "0.1.0-SNAPSHOT"
   :description "A clonable & upstream untangled template"
   :license {:name "MIT" :url "https://opensource.org/licenses/MIT"}
+  :min-lein-version "2.7.0"
 
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/clojurescript "1.9.671"]
